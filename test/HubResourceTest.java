287c677b7d5cac6acd618fee5a9cd3bbc91cba5d
fcas <f3l1nux@gmail.com>
2013-12-04 11:56:31 -0200

    Update HubResourceTest.java

--- test/HubResourceTest.java
+++ test/HubResourceTest.java
@@ -21,27 +21,27 @@ import org.junit.rules.ExpectedException;
  * @author felipe
  */
 public class HubResourceTest {
-    
+
     private HubResource hub = new HubResource();
     private LinkedList<String> topics;
     private SubscribeBean subscribeBean = new SubscribeBean();
     private BlockingQueue<SubscribeBean> subscriberQueue;
-    
+
     public HubResourceTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -50,42 +50,42 @@ public class HubResourceTest {
     //
     // @Test
     // public void hello() {}
-    
+
     @Test
-    public void testPublicarFail() throws ConnectException{
-        
-        try{
+    public void testPublicarFail() throws ConnectException {
+
+        try {
             hub.publicar(" ", " ");
-        } catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("Não é possível publicar tópico, ou, valor inválido");
         }
     }
-    
+
     @Test
-    public void testPublicar() throws ConnectException{   
+    public void testPublicar() throws ConnectException {
         topics = hub.getTopics();
         topics.add("001");
         hub.setTopics(topics);
         hub.publicar("001", "Úmido");
         assertTrue(hub.isSubscriberNotificado());
     }
-    
+
     @Test
-    public void testRegistrar(){
+    public void testRegistrar() {
         hub.registrar("002");
         topics = hub.getTopics();
         assertTrue(topics.contains("002"));
     }
-    
+
     @Test
-    public void testRegistrarFail(){
+    public void testRegistrarFail() {
         hub.registrar("002");
         hub.registrar("002");
         assertTrue(hub.isContainTopic());
     }
-    
+
     @Test
-    public void testSubscribe() throws InterruptedException{
+    public void testSubscribe() throws InterruptedException {
         topics = hub.getTopics();
         topics.add("001");
         hub.setTopics(topics);
@@ -93,24 +93,30 @@ public class HubResourceTest {
         hub.subscribe(subscribeBean);
         assertTrue(hub.isContainSubscribe());
     }
-    
+
     @Test
-    public void testSubscribeFail() throws InterruptedException{
+    public void testSubscribeFail() throws InterruptedException {
         topics = hub.getTopics();
         topics.add("002");
         hub.setTopics(topics);
         subscribeBean.setTopic("001");
         hub.subscribe(subscribeBean);
-        assertTrue(!hub.isContainSubscribe());  
+        assertTrue(!hub.isContainSubscribe());
     }
-    
+
     @Test
-    public void testUnsubscriber() throws InterruptedException{
+    public void testUnsubscriber() throws InterruptedException, ConnectException {
         topics = hub.getTopics();
         topics.add("001");
         hub.setTopics(topics);
         subscribeBean.setTopic("001");
-        hub.unsubscribe(subscribeBean);
+        try {
+            subscriberQueue = hub.getSubscriberQueue();
+            subscriberQueue.add(subscribeBean);
+            hub.setSubscriberQueue(subscriberQueue);
+            hub.unsubscribe(subscribeBean);
+        } catch (Exception e) {
+        }
         topics = hub.getTopics();
         topics.add("001");
         hub.setTopics(topics);
@@ -120,11 +126,11 @@ public class HubResourceTest {
         subscribeBean.setPort(8080);
         assertEquals(8080, subscribeBean.getPort());
         hub.subscribe(subscribeBean);
-        assertTrue(hub.isContainSubscribe());       
+        assertTrue(hub.isContainSubscribe());
     }
-    
+
     @Test()
-    public void notificar() throws ConnectException{
+    public void notificar() throws ConnectException {
         subscriberQueue = hub.getSubscriberQueue();
         topics = hub.getTopics();
         topics.add("001");
@@ -134,7 +140,4 @@ public class HubResourceTest {
         hub.publicar("001", "001");
         assertTrue(hub.isSubscriberNotificado());
     }
-    
 }
-    
-    
